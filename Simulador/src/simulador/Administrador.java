82f3f875f8cf23c8b3577fdc9fe58b74873e66bf
Diego <Diego@Diego>
2016-05-03 19:06:30 -0600

    Se agregaron los archivos que se crearon el lunes

---
+++ Simulador/src/simulador/Administrador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author b24882
+ */
+public class Administrador {
+    int hilos;      //Total de hilos que se van a correr
+    Contexto tablaDeRegistros[];
+    
+    Administrador(){
+        tablaDeRegistros = new Contexto[3];
+    }
+}
